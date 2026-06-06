db27a297a17bc5608ca63352101b462a7cf0c0ac
Guanmou___ <lushoufeng87@gmail.com>
2026-06-06 11:52:54 +0800

    Update 3neuron network.py

--- 3neuron network.py
+++ 3neuron network.py
@@ -1,4 +1,6 @@
-# 3 neurons, 1 inhibitory
+# small network → learning → sensory inputs → simple behaviors
+# 
+#  3 neurons, 1 inhibitory
 # Neuron 1 excites Neuron 2
 # Neuron 3 inhibits Neuron 2
 # Both Neuron 1 and Neuron 3 receive external input
@@ -13,7 +15,7 @@ a, b, c, d = 0.02, 0.2, -65, 8
 
 # neuron 1 (excitatory)
 v1 = -65
-u1 = b * v1
+u1 = b * v1 # fatigue for neurons, increases when neuron spikes, mirrors biological behavior
 
 # neuron 2 (target)
 v2 = -65
@@ -26,14 +28,14 @@ u3 = b * v3
 # synapses
 
 # N1 to N2 (excitatory)
-w12 = 4
-syn12 = 0
+w12 = 4 # each spike of neuron 1 produces 4 units of excitatory current in neuron 2
+syn12 = 0 # stores the current state of the synapse
 
 # N3 to N2 (inhibitory)
 w32 = -6
 syn32 = 0
 
-decay = 0.95
+decay = 0.95 #clearance of neutransmitters inside the synpatic cleft
 
 
 # inputs
@@ -65,12 +67,12 @@ for t in range(time_steps):
     I1 = I1_const
     I3 = I3_const
 
-    # target neuron receives both excitation and inhibition
+    # target neuron receives input only from other neurons, no external input
     I2 = syn12 + syn32
 
     # neuron 1
 
-    v1 += 0.04 * v1**2 + 5 * v1 + 140 - u1 + I1
+    v1 += 0.04 * v1**2 + 5 * v1 + 140 - u1 + I1 # differential equation for Izhikevich neuron model
     u1 += a * (b * v1 - u1)
 
     spike1 = False
@@ -90,7 +92,7 @@ for t in range(time_steps):
 
     # neuron 3
 
-    v3 += 0.04 * v3**2 + 5 * v3 + 140 - u3 + I3
+    v3 += 0.04 * v3**2 + 5 * v3 + 140 - u3 + I3 
     u3 += a * (b * v3 - u3)
 
     spike3 = False
